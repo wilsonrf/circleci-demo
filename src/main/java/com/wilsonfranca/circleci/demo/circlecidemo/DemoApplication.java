5ce549b2b07dda98db9549b0787ecd709a60a3a3
Wilson da Rocha França <wilsonrf@gmail.com>
2018-03-04 21:47:24 -0300

    App initlz

--- src/main/java/com/wilsonfranca/circleci/demo/circlecidemo/DemoApplication.java
+++ src/main/java/com/wilsonfranca/circleci/demo/circlecidemo/DemoApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Bla");
 		SpringApplication.run(DemoApplication.class, args);
+		
+		
 	}
 }
